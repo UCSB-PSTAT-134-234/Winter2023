f559a1f11cd5de8dfe9f5507ae291e62b9fcc7d5
Sang-Yun Oh <syoh@ucsb.edu>
2023-01-09 01:06:30 -0800

    lab1

---
+++ labs/lab1/student/tests/q3.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q3',
+    'points': 3,
+    'suites': [   {   'cases': [{'code': '>>> isinstance(z, int)\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> 0 < z < 100\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
