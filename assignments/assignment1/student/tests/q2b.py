609a7eacf2bab21cc68a2b0aeff23d6ce8aa22e6
Sang-Yun Oh <syoh@ucsb.edu>
2023-01-22 14:30:03 -0800

    assignment 1

---
+++ assignments/assignment1/student/tests/q2b.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2b',
+    'points': 3,
+    'suites': [   {   'cases': [{'code': '>>> \n>>> assert isinstance(rising_candidate, str) and isinstance(falling_candidate, str) \n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
