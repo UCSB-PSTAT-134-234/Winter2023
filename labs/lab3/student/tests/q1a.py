e32825d03821173f34e92b6c2d605ec83f8a8ae6
roobnloo <71776622+roobnloo@users.noreply.github.com>
2023-01-31 12:51:48 -0800

    modify tests to match new dataframe

--- labs/lab3/student/tests/q1a.py
+++ labs/lab3/student/tests/q1a.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q1a',
     'points': 3,
-    'suites': [   {   'cases': [{'code': '>>> calls_by_cvlegend_and_offense["LARCENY", "THEFT FROM PERSON"] == 24\nFalse', 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': '>>> calls_by_cvlegend_and_offense["LARCENY", "THEFT FROM PERSON"] == 13\nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
