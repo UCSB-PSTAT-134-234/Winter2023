8a6e9e8c0f20dd6c7db470f47e748c1822a74433
Sang-Yun Oh <syoh@ucsb.edu>
2023-02-21 01:28:46 -0800

    modeling and estimation

---
+++ labs/lab6/student/tests/q2b.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2b',
+    'points': 3,
+    'suites': [{'cases': [{'code': ">>> loss.dtype == np.dtype('float64')\nTrue", 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
