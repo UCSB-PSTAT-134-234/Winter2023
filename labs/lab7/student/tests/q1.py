73df65814a99e5f8ecb3c017acd4b57d16af1d51
Sang-Yun Oh <syoh@ucsb.edu>
2023-02-28 03:28:37 -0800

    lab7 cvxpy

---
+++ labs/lab7/student/tests/q1.py
@@ -0,0 +1,11 @@
+test = {   'name': 'q1',
+    'points': 3,
+    'suites': [   {   'cases': [   {'code': ">>> X1.columns\nIndex(['intercept', 'LSTAT'], dtype='object')", 'hidden': False, 'locked': False},
+                                   {'code': '>>> X1.shape\n(506, 2)', 'hidden': False, 'locked': False},
+                                   {'code': ">>> np.all(y == housing['MEDV'])\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': ">>> np.all(X1['intercept'] == 1)\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': ">>> np.all(X1['LSTAT'] == housing['LSTAT'])\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ labs/lab7/student/tests/q2.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2',
+    'points': 3,
+    'suites': [{'cases': [{'code': '>>> np.round(beta2.value, 3)\narray([34.554, -0.95 ])', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
