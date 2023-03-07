10239fc20eb0617902dc759b1cd6fcd5ae4bb1fd
roobnloo <71776622+roobnloo@users.noreply.github.com>
2023-03-06 18:47:01 -0800

    add lab 8

---
+++ labs/lab8/student/tests/q2b.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q2b',
+    'points': 3,
+    'suites': [{'cases': [{'code': ">>> res_q2b\n'Python:Exercises: PHP exercises.'", 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
